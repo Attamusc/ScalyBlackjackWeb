bcb15c07b2e5191d91b3e1de443532ed3f284f6a
Sean Dunn <sean@attamusc.com>
2011-10-19 02:44:04 -0400

    Modified the templates to be more modular and lift-esc; Added a Navigation Generator snippet

---
+++ src/main/scala/code/snippet/NavBar.scala
@@ -0,0 +1,22 @@
+package code 
+package snippet 
+
+import scala.xml.{NodeSeq, Text}
+import code.lib._
+import lib._
+import net.liftweb._
+import http._
+import common._
+import util.Helpers._
+
+class NavBar {
+  	def navGen = {
+		val isLoggedIn = S.attr("isLoggedIn").openOr("")
+		Logger.debug("the attribute is: " + isLoggedIn)
+		isLoggedIn match {
+			case "false" => "*" #> <lift:embed what="base_nav" />
+			case "true" => "*" #> "<lift:embed what=\"logged_in_nav\">"
+			case _ => "*" #> "<div>Whoops!!!</div>"
+		}
+	}
+}
