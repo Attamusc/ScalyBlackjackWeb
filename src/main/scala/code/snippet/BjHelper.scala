90ea044945f6c52e6d045095fb1b2801613234ff
Sean Dunn <sean@attamusc.com>
2011-11-30 21:37:34 -0500

    Added in prettier ajax buttons and an ajax bet

--- src/main/scala/code/snippet/BjHelper.scala
+++ src/main/scala/code/snippet/BjHelper.scala
@@ -4,17 +4,28 @@ package snippet
 import net.liftweb.http._
 import net.liftweb.http.rest._
 import net.liftweb.json.JsonAST._
-import net.liftweb.json._
 import net.liftweb.common.{Box,Full,Empty,Failure,ParamFailure}
 import net.liftweb.mapper._
+import js._
+import JsCmds._
+import JE._
 
 import lib.bj.Game
+import comet.Conductor
 
 object BjHelper extends RestHelper {
     serve {
         case "api" :: "bj" :: _ Get _ =>
             Game.init
             RedirectResponse("/")
+        case "api" :: "bj" :: "bet" :: _ Post _ =>
+            val message = "(action: %s, pid: %s, tid: %s, amount: %s)".format("bet", S.param("pid").openOr(""), S.param("tid").openOr(""), S.param("amount").openOr(""))
+            Conductor ! message
+            JString("Ok")
+        case "api" :: "bj" :: player_action :: _ Post _ =>
+            val message = "(action: %s, pid: %s, tid: %s)".format(player_action, S.param("pid").openOr(""), S.param("tid").openOr(""))
+            Conductor ! message
+            JString("Ok")
     }
 }
 
